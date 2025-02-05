910cd26f66b27cdfe569a32995b8d7497c5bf6cc
Dev-Ridwan16 <devridwan1@gmail.com>
2025-02-05 02:33:26 +0000

    bug fixes

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ app = Flask(__name__)
 BASE_DIR = os.path.abspath(os.path.dirname(__file__))
 
 app.config["SQLALCHEMY_DATABASE_URI"] = (
-    f"sqlite:///{os.path.join(BASE_DIR, "database.db")}"
+    f"sqlite:///{os.path.join(BASE_DIR, 'database.db')}"
 )
 app.config["SECRET_KEY"] = "valentine_secret_key"
 app.config["UPLOADED_IMAGES_DEST"] = (
